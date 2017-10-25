a8c464ea49c6a025035b81680309b3f1d35d0867
Darius Telyčėnas <darius.telycenas@spryker.com>
2017-10-25 12:05:20 +0200

    ECO-837 Eco CI plan
    - Tweak travis configuration and scripts

--- build/travis.sh
+++ build/travis.sh
@@ -4,30 +4,90 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 buildResult=1
 buildMessage=""
+result=0
 
 function runTests {
-    grep APPLICATION_ROOT_DIR "$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/composer/autoload_real.php"
-    if [ "$?" = 1 ]; then
-        echo "define('APPLICATION_ROOT_DIR', '$TRAVIS_BUILD_DIR/$SHOP_DIR');" >> "$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/composer/autoload_real.php"
+    "$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/bin/console" transfer:generate
+    if [ "$?" = 0 ]; then
+        buildMessage="${buildMessage}\n${GREEN}Transfer objects generation was successful"
+    else
+        buildMessage="${buildMessage}\n${RED}Transfer objects generation was not successful"
+        result=$((result+1))
+    fi
+
+    "$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/bin/console" propel:install
+    if [ "$?" = 0 ]; then
+        buildMessage="${buildMessage}\n${GREEN}Propel models generation was successful"
+    else
+        buildMessage="${buildMessage}\n${RED}Propel models generation was not successful"
+        result=$((result+1))
     fi
 
     echo "Setup test environment..."
-    "$TRAVIS_BUILD_DIR/$SHOP_DIR/setup_test" -f
+    ./setup_test -f
 
     echo "Running tests..."
-    "$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/bin/codecept" build -c "vendor/spryker-eco/$MODULE_NAME"
-    "$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/bin/codecept" run -c "vendor/spryker-eco/$MODULE_NAME"
+    "$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/bin/codecept" build -c "vendor/spryker-eco/$MODULE_NAME/"
+    "$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/bin/codecept" run -c "vendor/spryker-eco/$MODULE_NAME/"
     if [ "$?" = 0 ]; then
         buildMessage="${buildMessage}\n${GREEN}Tests are passing"
-        result=0
     else
         buildMessage="${buildMessage}\n${RED}Tests are failing"
-        result=1
+        result=$((result+1))
     fi
     cd "$TRAVIS_BUILD_DIR/$SHOP_DIR"
 
     echo "Tests finished"
     return $result
+}
+
+function checkArchRules {
+    echo "Running Architecture sniffer..."
+    errors=`vendor/bin/phpmd "vendor/spryker-eco/$MODULE_NAME/src" text vendor/spryker/architecture-sniffer/src/ruleset.xml --minimumpriority=2`
+    errorsCount=`echo "$errors" | wc -l`
+
+    if [[ "$errorsCount" = "0" ]]; then
+        buildMessage="$buildMessage\n${GREEN}Architecture sniffer reports no errors"
+    else
+        echo -e "$errors"
+        buildMessage="$buildMessage\n${RED}Architecture sniffer reports $errorsCount error(s)"
+    fi
+}
+
+function checkCodeSniffRules {
+    licenseFile="$TRAVIS_BUILD_DIR/.license"
+    if [ -f "$licenseFile" ]; then
+        echo "Preparing correct license for code sniffer..."
+        cp "$licenseFile" "$TRAVIS_BUILD_DIR/$SHOP_DIR/.license"
+    fi
+
+    echo "Running code sniffer..."
+    errors=`vendor/bin/console code:sniff "vendor/spryker-eco/$MODULE_NAME/src"`
+    errorsCount=$?
+
+    if [[ "$errorsCount" = "0" ]]; then
+        buildMessage="$buildMessage\n${GREEN}Code sniffer reports no errors"
+    else
+        echo -e "$errors"
+        buildMessage="$buildMessage\n${RED}Code sniffer reports some error(s)"
+    fi
+}
+
+function checkPHPStan {
+    echo "Installing PHPStan..."
+    composer require --dev phpstan/phpstan
+    echo "Updating code-completition..."
+    vendor/bin/console dev:ide:generate-auto-completion
+    echo "Running PHPStan..."
+    errors=`php -d memory_limit=2048M vendor/bin/phpstan analyze -c phpstan.neon "vendor/spryker-eco/$MODULE_NAME/src" -l 2`
+
+    errorsCount=`echo "$errors" | wc -l`
+    if [[ "$errorsCount" = "0" ]]; then
+        buildMessage="$buildMessage\n${GREEN}PHPStan reports no errors"
+    else
+        echo -e "$errors"
+        buildMessage="$buildMessage\n${RED}PHPStan reports some error(s)"
+    fi
 }
 
 function checkWithLatestDemoShop {
@@ -71,8 +131,19 @@ function checkModuleWithLatestVersionOfDemoShop {
     fi
 }
 
+updatedFile="$TRAVIS_BUILD_DIR/$SHOP_DIR/vendor/codeception/codeception/src/Codeception/Application.php"
+grep APPLICATION_ROOT_DIR "$updatedFile"
+if [ $? = 1 ]; then
+    echo "define('APPLICATION_ROOT_DIR', '$TRAVIS_BUILD_DIR/$SHOP_DIR');" >> "$updatedFile"
+fi
+
 cd $SHOP_DIR
 checkWithLatestDemoShop
+if [ -d "vendor/spryker-eco/$MODULE_NAME/src" ]; then
+    checkArchRules
+    checkCodeSniffRules
+    checkPHPStan
+fi
 
 echo -e "$buildMessage"
 exit $buildResult
